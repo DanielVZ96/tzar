76ae9d834fe44fc6b52ec5c3d5f2a7f50daf17ba
Daniel Valenzuela <daniel@admetricks.com>
2021-08-19 08:58:26 -0400

    🚩 0.1.3: Version flag and argument aliases

--- tzar/__init__.py
+++ tzar/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.1.0'
+__version__ = '0.1.3'
 __author__ = 'Daniel Valenzuela'
 __licence__ = 'MIT'
--- tzar/cli.py
+++ tzar/cli.py
@@ -3,12 +3,34 @@ import click
 from tzar.templates import CLITemplateCollection, CLIArguments
 
 
-@click.group()
+class AliasedGroup(click.Group):
+    def get_command(self, ctx, cmd_name):
+        rv = click.Group.get_command(self, ctx, cmd_name)
+        if rv is not None:
+            return rv
+        matches = [x for x in self.list_commands(ctx) if x.startswith(cmd_name)]
+        if not matches:
+            return None
+        elif len(matches) == 1:
+            return click.Group.get_command(self, ctx, matches[0])
+        ctx.fail(f"Too many matches: {', '.join(sorted(matches))}")
+
+    def resolve_command(self, ctx, args):
+        # always return the full command name
+        _, cmd, args = super().resolve_command(ctx, args)
+        return cmd.name, cmd, args
+
+
+@click.group(
+    cls=AliasedGroup,
+    epilog="Tip: You can run any subcommand by only using it's first letter(s). E.g. instead of `extract`, just `e`, `ex`, `ext`, etc.",
+)
+@click.version_option("0.1.3", "--version", "-V")
 def cli():
     pass
 
 
-@cli.command()
+@cli.command(no_args_is_help=True)
 @click.argument("source")
 @click.argument("destination")
 @click.option("--verbose", "-v", is_flag=True, help="Print more output.")
@@ -21,7 +43,7 @@ def extract(source, destination, verbose, extension):
     )
 
 
-@cli.command()
+@cli.command(no_args_is_help=True)
 @click.argument("source")
 @click.argument("destination")
 @click.option("--verbose", "-v", is_flag=True, help="Print more output.")
@@ -39,7 +61,7 @@ def compress(source, destination, verbose, extension):
     )
 
 
-@cli.command()
+@cli.command(no_args_is_help=True)
 @click.argument("source")
 @click.option("--verbose", "-v", is_flag=True, help="Print more output.")
 @click.option(
@@ -54,8 +76,10 @@ def list(source, verbose, extension):
         )
     )
 
+
 def run():
     cli()
+
 
 if __name__ == "__main__":
     run()
